5a7fc202b5b8221d1be3347e83037009ba95fe5b
Daniel Schüssler <daniels@community.haskell.org>
2015-02-08 17:47:35 +0100

    Test suite: Put types into separate module to fix "‘E’ is not in the type environment at a reify"

--- testing/Main.hs
+++ testing/Main.hs
@@ -8,33 +8,7 @@ import Language.Haskell.TH.ExpandSyns
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import Util
-    
--- type A = forall a. B a; type B a = Maybe a; expand [t|B A|]
-
-type ListOf x = [x] 
-type ForAll f = forall x. f x 
-type ApplyToInteger f = f Integer
-type Int' = Int
-type Either' = Either
-type Int'' = Int
-
-$(sequence [tySynD (mkName "E") [PlainTV (mkName "x")]  
-                (forallT'' ["y"] (conT ''Either `appT` varT' "x" `appT` varT' "y" --> conT ''Int))
-           ])
-
-
-data family DF1 a
-
-data instance DF1 Int = DInt (ListOf ())
-
-type family TF1 a
-
-type instance TF1 Int = ListOf ()
-
-class Class1 a where
-    type AT1 a
-
-instance Class1 Int where type AT1 Int = ListOf ()
+import Types    
 
 
 main = do
