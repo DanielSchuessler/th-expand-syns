b0fee0f8a6a7023a512c91f50d13e5a324c66130
Daniel Schüssler <daniels@community.haskell.org>
2016-11-19 21:25:26 +0100

    Fix test for GHC 7.4

--- testing/Main.hs
+++ testing/Main.hs
@@ -9,22 +9,25 @@ import Language.Haskell.TH.ExpandSyns
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import Util
-import Types    
+import Types
 
+#if !MIN_VERSION_template_haskell(2,8,0)
+reportWarning = report False
+#endif
 
 main = do
     putStrLn "Basic test..."
-    $(mkTest  [t| forall a. Show a => a -> ForAll [] -> (Int,ApplyToInteger []) |] 
+    $(mkTest  [t| forall a. Show a => a -> ForAll [] -> (Int,ApplyToInteger []) |]
 
--- GHC 7.8 always seems to consider the body of 'ForallT' to have a 'PlainTV', 
--- whereas it always has a 'KindedTV' with GHC 7.10 (in both cases, it doesn't appear 
+-- GHC 7.8 always seems to consider the body of 'ForallT' to have a 'PlainTV',
+-- whereas it always has a 'KindedTV' with GHC 7.10 (in both cases, it doesn't appear
 -- to matter whether the definition of 'ForAll' is actually written with a kind signature).
 #if MIN_VERSION_template_haskell(2,10,0)
               [t| forall a. Show a => a -> (forall (x :: *). [] x) -> (Int,[] Integer) |]
 #else
               [t| forall a. Show a => a -> (forall x. [] x) -> (Int,[] Integer) |]
 #endif
-              
+
               )
 
     putStrLn "Variable capture avoidance test..."
@@ -38,15 +41,15 @@ main = do
 #endif
 
         expectedExpansion =
-         forallT 
-            [y_0] 
+         forallT
+            [y_0]
             (cxt [])
             (conT ''Either `appT` varT' "y" `appT` varT' "y_0" --> conT ''Int)
 
          -- the naive (and wrong) result would be:
          --   forall y. (forall y. Either y y -> Int)
       in
-        mkTest  (forallT'' ["y"] (conT' "E" `appT` varT' "y")) 
+        mkTest  (forallT'' ["y"] (conT' "E" `appT` varT' "y"))
                 (forallT'' ["y"] expectedExpansion))
 
     putStrLn "Testing that it doesn't crash on type families (expanding them is not supported yet)"
@@ -54,18 +57,18 @@ main = do
         t = [t| (DF1 Int, TF1 Int, AT1 Int) |]
       in
         mkTest t t)
-             
-    putStrLn "Testing that the args of type family applications are handled" 
+
+    putStrLn "Testing that the args of type family applications are handled"
     $(mkTest [t| (DF1 Int', TF1 Int', AT1 Int') |]
              [t| (DF1 Int, TF1 Int, AT1 Int) |])
 
     putStrLn "Higher-kinded synonym"
-    $(mkTest 
+    $(mkTest
         [t| Either' (ListOf Int') (ListOf Char) |]
         [t| Either [Int] [Char] |])
 
     putStrLn "Nested"
-    $(mkTest 
+    $(mkTest
         [t| Int'' |]
         [t| Int |])
 
