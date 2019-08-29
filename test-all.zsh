22106be8867d09f9f23a153a544e4cf064fee510
Daniel Schüssler <933504+DanielSchuessler@users.noreply.github.com>
2019-08-29 23:46:38 +0200

    Update test-all script (add GHC 8.4-8.8; remove GHC < 8)

--- test-all.zsh
+++ test-all.zsh
@@ -11,7 +11,8 @@ go() {
     echo
 }
 
-go --resolver lts-2  # GHC 7.8.4
-go --resolver lts-6  # GHC 7.10.3
 go --resolver lts-9  # GHC 8.0.2
 go --resolver lts-10 # GHC 8.2.2
+go --resolver lts-12 # GHC 8.4.4
+go --resolver lts-14 # GHC 8.6.5
+go --stack-yaml stack-8.8.yaml
